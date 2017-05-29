8889c201174213da8acde8d67d82d5c1a215e8c7
Lianleli <lianleli@hotmail.com>
2017-05-29 11:16:20 +0200

    Update config.js

--- app/config/env/config.js
+++ app/config/env/config.js
@@ -1,8 +1,8 @@
 angular.module("duadvisor.env", [])
 .constant("ENV", {
- "environment": "stage",
- "backendEndpoint": "http://stage.api.duadvisor.hr/public",
+ "environment": "production",
+ "backendEndpoint": "http://api.duadvisor.hr/public",
  "enableLogging": true,
- "s3UploadBucket": "duadvisor-files-stage",
+ "s3UploadBucket": "duadvisor-files",
  "googleMapsApiKey": "AIzaSyBjAIooUetXhVZ5r9_t50NU8C_PE_vIrTM"
 });
